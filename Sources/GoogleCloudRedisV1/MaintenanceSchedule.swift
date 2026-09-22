8a6d6135b8b4abca9ae52a1c511a99899d64a0a0
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:01:37 -0400

    chore: update librarian with suppressed diagnostics (#1014)
    In this version of librarian all the 'DeprecatedDeclaration' warnings
    are supressed.
    
    We want these diagnostics because they inform customers when they are
    trying to use a service, method, message, field, enum or enum value that
    has been deprecated.
    
    We also need to use the same elements, if nothing else, to return them
    when the service includes them in a response, or allow customers to keep
    using them until they have time to fix their code.
    
    Until Swift 6.4 we could not supress the diagnostics, which was too
    noisy. With this PR the diagnostics are suppressed, and the CI system
    now can enable warnings for other locations (say examples) in case we
    use the deprecated elements where we shouldn't.
    
    On Swift 6.2 and 6.3, the diagnostics are not suppressed. Not ideal. On
    the other hand, we expect those to go away in 18 months or so.

--- Sources/GoogleCloudRedisV1/MaintenanceSchedule.swift
+++ Sources/GoogleCloudRedisV1/MaintenanceSchedule.swift
@@ -75,6 +75,9 @@ public struct MaintenanceSchedule: Codable, Equatable, GoogleWKT._AnyPackable,
     ]
   }
 
+  #if hasAttribute(diagnose)
+    @diagnose(DeprecatedDeclaration, as: ignored)
+  #endif
   public init(from decoder: Decoder) throws {
     let container = try decoder.container(keyedBy: CodingKeys.self)
     self.startTime = try container.decodeIfPresent(GoogleWKT.Timestamp.self, forKey: .startTime)
@@ -90,6 +93,9 @@ public struct MaintenanceSchedule: Codable, Equatable, GoogleWKT._AnyPackable,
     }
   }
 
+  #if hasAttribute(diagnose)
+    @diagnose(DeprecatedDeclaration, as: ignored)
+  #endif
   public func encode(to encoder: Encoder) throws {
     var container = encoder.container(keyedBy: CodingKeys.self)
     try container.encodeIfPresent(self.startTime, forKey: .startTime)
